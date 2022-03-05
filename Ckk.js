fcb67932fcfdb4523c4e819ecd3710291b3e5ae1
mycecilion <100381034+mycecilion@users.noreply.github.com>
2022-03-06 01:32:12 +0700

    Create Ckk.js

---
+++ Ckk.js
@@ -0,0 +1,25 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("vipsht");
+    if (user != "") {
+        document.getElementById('ads').style.display="";
+    } else {
+       document.getElementById('ads').style.display="none";
+       
+       }
+    }
+}
