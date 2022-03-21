cad17e5b7a58851c6285ce7b451f168ceb5292cd
mycecilion <100381034+mycecilion@users.noreply.github.com>
2022-03-21 18:00:40 +0700

    Create Ngambek.js

---
+++ Ngambek.js
@@ -0,0 +1,48 @@
+if(window.location.href.indexOf("#?o=1") != -1){
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("dcsafe").style.display="none";document.getElementById("dcgoto").style.display="";
+  } else {
+    document.getElementById("dcsafe").innerHTML = timeleft + typo;
+  }
+  timeleft -= 1;
+}, 1000);
+  
+	document.getElementById('dcsafe').style.display="";
+}
+  
+  
+  function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function dcgo(cname) {
+ let user = getCookie("dcsafe");
+  if (user != "") {
+    window.location.href=(user);
+  }
+}
+  
+function dcgoto() {
+document.getElementById('dcgo').style.display="";document.getElementById('dcgoto').style.display="none";
+  }
+  
