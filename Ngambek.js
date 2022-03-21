671589ff5981bde8e6474bacae76d4b3f0e58f72
mycecilion <100381034+mycecilion@users.noreply.github.com>
2022-03-21 18:27:57 +0700

    Update Ngambek.js

--- Ngambek.js
+++ Ngambek.js
@@ -1,29 +1,28 @@
-if(window.location.href.indexOf("#?o=1") != -1){
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("dcsafe").style.display="none";document.getElementById("dcgoto").style.display="";
-  } else {
-    document.getElementById("dcsafe").innerHTML = timeleft + typo;
-  }
-  timeleft -= 1;
-}, 1000);
-  
-	document.getElementById('dcsafe').style.display="";
+if (window.location.href.indexOf('#?o=1') != -1) {
+  var downloadTimer = setInterval(() => {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      document.getElementById('dcsafe').style.display = 'none'; document.getElementById('dcgoto').style.display = '';
+    } else {
+      document.getElementById('dcsafe').innerHTML = timeleft + typo;
+    }
+    timeleft -= 1;
+  }, 1000);
+
+  document.getElementById('dcsafe').style.display = '';
 }
-  
-  
-  function setCookie(cname, cvalue, exdays) {
+
+function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  const expires = `expires=${d.toUTCString()}`;
+  document.cookie = `${cname}=${cvalue};${expires};path=/`;
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
+  const name = `${cname}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -32,17 +31,16 @@ function getCookie(cname) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return '';
 }
 
 function dcgo(cname) {
- let user = getCookie("dcsafe");
-  if (user != "") {
-    window.location.href=(user);
+  const user = getCookie('dcsafe');
+  if (user != '') {
+    window.location.href = (user);
   }
 }
-  
+
 function dcgoto() {
-document.getElementById('dcgo').style.display="";document.getElementById('dcgoto').style.display="none";
-  }
-  
+  document.getElementById('dcgo').style.display = ''; document.getElementById('dcgoto').style.display = 'none';
+}
