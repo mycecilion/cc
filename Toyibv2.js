be33aa1936593386db91727ac010cc34261916be
mycecilion <100381034+mycecilion@users.noreply.github.com>
2022-04-09 11:03:41 +0700

    Create Toyibv2.js

---
+++ Toyibv2.js
@@ -0,0 +1,2 @@
+if(window.location.href.indexOf("#?o=") != -1){ document.getElementById('adsByDC').innerHTML="<div id='timer'/><div style='text-align: center'><button class='bt-success hidden' disabled='' id='wcGetLink'>Lanjut</button></div>";document.getElementById('adsByDC2').innerHTML="<div style='text-align: center'><button class='bt-success hidden' disabled='' id='gotolink' onclick='ads1();'>Sini Bang</button></div>";
+}
